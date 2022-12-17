d78384ca212b82ef2a79b9395c91fd2d0914ce79
Xcreen <Xcreen@users.noreply.github.com>
2022-12-17 17:17:58 +0100

    Implement listing host-entries

--- src/components/HostFileEditor.vue
+++ src/components/HostFileEditor.vue
@@ -8,6 +8,9 @@ export default {
       inputAddIP: '',
       inputAddDomain: '',
       inputAddDescription: '',
+      inputEditIPs: [],
+      inputEditDomain: [],
+      inputEditDescription: [],
       cleanLines: []
     }
   },
@@ -23,12 +26,37 @@ export default {
       hostFileLines.forEach((line, index) => {
         const tmpLine = line.trim();
         if(tmpLine.length > 0 && !tmpLine.startsWith('#')) {
+          const lineArr = tmpLine.split(" ");
           const tmpData = {};
           tmpData.originalLine = line;
           tmpData.cleanLine = tmpLine;
           tmpData.lineNumber = index + 1;
+          tmpData.ip = '';
+          tmpData.domain = '';
+          tmpData.description = '';
+          if(lineArr.length > 0) {
+            tmpData.ip = lineArr[0];
+            if(lineArr.length > 1) {
+              tmpData.domain = lineArr[1];
+            }
+            if(lineArr.length > 3) {
+              if(lineArr[2] === '#') {
+                let tmpDescription = '';
+                for(let i = 3; i < lineArr.length; i++) {
+                  tmpDescription += lineArr[i] + ' ';
+                }
+                tmpData.description = tmpDescription;
+              }
+            }
+          }
           this.cleanLines.push(tmpData);
         }
+      });
+
+      this.cleanLines.forEach((lineData, index) => {
+        this.inputEditIPs[index] = lineData.ip;
+        this.inputEditDomain[index] = lineData.domain;
+        this.inputEditDescription[index] = lineData.description;
       });
     },
     async addHostEntry() {
@@ -67,8 +95,29 @@ export default {
 <template>
   <div class="host-file-entry-wrapper">
     <button class="btn btn-primary mt-2" data-bs-toggle="modal" data-bs-target="#add-entry-modal">Add Host-Entry</button>
+    <div class="row">
+      <div class="col-sm-3">
+        <label>IP</label>
+      </div>
+      <div class="col-sm-3">
+        <label>Domain</label>
+      </div>
+      <div class="col-sm-3">
+        <label>Description</label>
+      </div>
+    </div>
 
-
+    <div class="row mb-3" v-for="(line, index) in cleanLines">
+      <div class="col-sm-3">
+        <input type="text" class="form-control" v-model="inputEditIPs[index]">
+      </div>
+      <div class="col-sm-3">
+        <input type="text" class="form-control" v-model="inputEditDomain[index]">
+      </div>
+      <div class="col-sm-3">
+        <input type="text" class="form-control" v-model="inputEditDescription[index]">
+      </div>
+    </div>
   </div>
 
   <!-- Add-Entry-Modal -->
