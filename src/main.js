a3ab606dc41eadfd422554879c35ff5b78d1a70e
Xcreen <Xcreen@users.noreply.github.com>
2022-12-24 00:02:07 +0100

    Add mitt for event-bus

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import { createApp } from "vue";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import "@fortawesome/fontawesome-free/css/all.min.css"
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App.vue";
+import mitt from 'mitt';
 
-createApp(App).mount("#app");
+const emitter = mitt();
+const app = createApp(App);
+
+app.config.globalProperties.emitter = emitter;
+app.mount("#app");
