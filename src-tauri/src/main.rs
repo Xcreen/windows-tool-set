13e86d728f58ac074b6b971f54dc7e1581520179
Xcreen <Xcreen@users.noreply.github.com>
2022-12-29 16:55:56 +0100

    Removed unnecessary extra namespace

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,17 +6,16 @@
 mod mod_host_file;
 
 use std::io::{prelude::*};
-use mod_host_file::mod_host_file::*;
 
 #[tauri::command]
 fn read_host_file() -> String {
-    return read_host_file_content();
+    return mod_host_file::read_host_file_content();
 }
 
 #[tauri::command]
 fn read_host_file_lines() -> Vec<String> {
     let mut lines = Vec::new();
-    let buf_reader = get_host_file_bufreader();
+    let buf_reader = mod_host_file::get_host_file_bufreader();
     for line in buf_reader.lines() {
         lines.push(line.unwrap());
     }
@@ -25,7 +24,7 @@ fn read_host_file_lines() -> Vec<String> {
 
 #[tauri::command]
 fn save_host_file(host_content: &str) -> bool {
-    let save_result = save_content_host_file(host_content);
+    let save_result = mod_host_file::save_content_host_file(host_content);
     match save_result {
         Ok(_val) => {
             return true;
@@ -48,7 +47,7 @@ fn delete_entry_from_host_file(delete_line: i32) -> bool {
     let mut lines = Vec::new();
     let mut line_counter = 1;
     let mut new_host_content = String::new();
-    let buf_reader = get_host_file_bufreader();
+    let buf_reader = mod_host_file::get_host_file_bufreader();
     for line in buf_reader.lines() {
         if line_counter != delete_line {
             lines.push(line.unwrap() + "\n");
@@ -64,7 +63,7 @@ fn delete_entry_from_host_file(delete_line: i32) -> bool {
 
 #[tauri::command]
 fn edit_host_line(line_no: usize, new_host_entry: &str) -> bool {
-    let save_result = edit_line(line_no, new_host_entry);
+    let save_result = mod_host_file::edit_line(line_no, new_host_entry);
     match save_result {
         Ok(..) => {
             return true;
--- src-tauri/src/mod_host_file.rs
+++ src-tauri/src/mod_host_file.rs
@@ -1,57 +1,55 @@
-pub mod mod_host_file {
-    use std::fs;
-    use std::fs::File;
-    use std::io::{Write};
-    use std::io::{self, BufReader};
+use std::fs;
+use std::fs::File;
+use std::io::{Write};
+use std::io::{self, BufReader};
 
-    const DEFAULT_HOST_FILE_PATH: &str = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+const DEFAULT_HOST_FILE_PATH: &str = "C:\\Windows\\System32\\drivers\\etc\\hosts";
 
-    pub fn open_host_file() -> io::Result<File> {
-        let f = File::open(DEFAULT_HOST_FILE_PATH)?;
-        Ok(f)
-    }
+pub fn open_host_file() -> io::Result<File> {
+    let f = File::open(DEFAULT_HOST_FILE_PATH)?;
+    Ok(f)
+}
 
-    pub fn read_host_file_content() -> String {
-        let file_content = fs::read_to_string(DEFAULT_HOST_FILE_PATH);
-        match file_content {
-            Ok(val) => {
-                return val;
-            },
-            Err(_err) => {
-                return "".to_string();
-            }
+pub fn read_host_file_content() -> String {
+    let file_content = fs::read_to_string(DEFAULT_HOST_FILE_PATH);
+    match file_content {
+        Ok(val) => {
+            return val;
+        },
+        Err(_err) => {
+            return "".to_string();
         }
     }
+}
 
 
-    pub fn get_host_file_bufreader() -> BufReader<File> {
-        let file_result = open_host_file();
-        match file_result {
-            Ok(val) => {
-                return BufReader::new(val);
-            },
-            Err(err) => {
-                panic!("Failed to create bufreader! {}", err);
-            }
+pub fn get_host_file_bufreader() -> BufReader<File> {
+    let file_result = open_host_file();
+    match file_result {
+        Ok(val) => {
+            return BufReader::new(val);
+        },
+        Err(err) => {
+            panic!("Failed to create bufreader! {}", err);
         }
     }
+}
 
-    pub fn save_content_host_file(content: &str) -> io::Result<()> {
-        let mut f = File::options().write(true).truncate(true).open(DEFAULT_HOST_FILE_PATH)?;
-        f.write_all(content.to_string().as_bytes())?;
-        f.sync_data()?;
-        Ok(())
-    }
+pub fn save_content_host_file(content: &str) -> io::Result<()> {
+    let mut f = File::options().write(true).truncate(true).open(DEFAULT_HOST_FILE_PATH)?;
+    f.write_all(content.to_string().as_bytes())?;
+    f.sync_data()?;
+    Ok(())
+}
 
-    pub fn edit_line(line_no: usize, new_host_entry: &str) -> io::Result<()> {
-        let contents = read_host_file_content();
-        let mut lines: Vec<&str> = contents.lines().collect();
-        lines[line_no - 1] = new_host_entry;
-        let new_contents = lines.join("\n");
+pub fn edit_line(line_no: usize, new_host_entry: &str) -> io::Result<()> {
+    let contents = read_host_file_content();
+    let mut lines: Vec<&str> = contents.lines().collect();
+    lines[line_no - 1] = new_host_entry;
+    let new_contents = lines.join("\n");
 
-        let mut f = File::options().write(true).truncate(true).open(DEFAULT_HOST_FILE_PATH)?;
-        f.write_all(new_contents.to_string().as_bytes())?;
-        f.sync_data()?;
-        Ok(())
-    }
+    let mut f = File::options().write(true).truncate(true).open(DEFAULT_HOST_FILE_PATH)?;
+    f.write_all(new_contents.to_string().as_bytes())?;
+    f.sync_data()?;
+    Ok(())
 }
