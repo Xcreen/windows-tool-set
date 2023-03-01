7e0a8cfa3170e6a733b786a9788e97c109fb79c0
Xcreen <Xcreen@users.noreply.github.com>
2023-03-01 21:23:09 +0100

    Adjust Bootstrap-Colors

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
-import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./style.css";
 import App from "./App.vue";
 import mitt from 'mitt';
 
